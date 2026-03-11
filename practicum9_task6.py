90096931702cdc642de2ab4f29aa32650a6e9aba
ivanova-12 <ivanov.sim-1977@yandex.ru>
2026-03-11 20:22:40 +0700

    Add files via upload

---
+++ practicum9_task6.py
@@ -0,0 +1,16 @@
+with open('all_f.txt', 'r', encoding='utf-8') as f:
+    number = f.readline()
+    try:
+        number = int(number)
+    except ValueError:
+        print('ERROR')
+        exit()
+    all_other_lines = f.readlines()
+    with open('input.txt', 'w', encoding='utf-8') as fi:
+        if len(all_other_lines) == number:
+            fi.write('YES')
+        else:
+            fi.write('NO')
+
+
+
---
+++ practicum9_task7.py
@@ -0,0 +1,11 @@
+with open('all_f.txt', 'r', encoding='utf-8') as f:
+    all_numbers = f.readlines()
+    for num in all_numbers:
+        if int(num) == 100:
+            all_numbers.remove(num)
+        with open('all_f.txt', 'w', encoding='utf-8') as f:
+            for num in all_numbers:
+                f.write(num)
+
+
+
