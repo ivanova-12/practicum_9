8826dc7dc2f536aa6acc1e1fe7138508193b2576
ivanova-12 <ivanov.sim-1977@yandex.ru>
2026-03-11 21:42:35 +0700

    Update practicum9_task1.py

--- practicum9_task1.py
+++ practicum9_task1.py
@@ -1,13 +1,10 @@
-with open('input.txt', 'w+', encoding='utf-8') as f:
-    f.writelines(input())
-    f.seek(0)
-    new_lines = []
-    for line in f:
-        new_line = line.upper()
-        new_lines.append(new_line)
+with open('input.txt', 'r', encoding='utf-8') as f:
+    all_lines = f.readlines()
+    with open('output.txt', 'w', encoding='utf-8') as fi:
+        for line in all_lines:
+            new_line = line.upper()
+            fi.write(new_line)
 
-with open('output.txt', 'w', encoding='utf-8') as fi:
-    fi.writelines(new_lines)
 
 
 
