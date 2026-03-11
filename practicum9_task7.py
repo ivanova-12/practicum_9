e276a9243a460e7775ef73f72f49aef7465cfbff
ivanova-12 <ivanov.sim-1977@yandex.ru>
2026-03-11 20:25:48 +0700

    Update practicum9_task7.py

--- practicum9_task7.py
+++ practicum9_task7.py
@@ -1,11 +1,12 @@
-with open('all_f.txt', 'r', encoding='utf-8') as f:
+with open('input.txt', 'r', encoding='utf-8') as f:
     all_numbers = f.readlines()
     for num in all_numbers:
         if int(num) == 100:
             all_numbers.remove(num)
-        with open('all_f.txt', 'w', encoding='utf-8') as f:
+        with open('input.txt', 'w', encoding='utf-8') as f:
             for num in all_numbers:
                 f.write(num)
+
 
 
 
