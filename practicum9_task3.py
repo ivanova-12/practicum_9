202e95cc804b6301d753b59139ebb018898df934
ivanova-12 <ivanov.sim-1977@yandex.ru>
2026-03-11 21:51:34 +0700

    Update practicum9_task3.py

--- practicum9_task3.py
+++ practicum9_task3.py
@@ -1,17 +1,13 @@
-with open('input.txt', 'w+', encoding='utf-8') as f:
-    while True:
-        s = input()
-        f.write(s + '\n')
-        if s == '':
-            break
-    f.seek(0)
-    new_lines = []
+with open('input.txt', 'r', encoding='utf-8') as f:
+    all_lines = f.readlines()
     end = ''
-    for line in f:
+    for line in all_lines:
         end += line[0]
 
 with open('output.txt', 'w', encoding='utf-8') as f:
     f.write(end)
+
+
 
 
 
