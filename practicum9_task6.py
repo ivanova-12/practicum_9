938b695cbb198423dd9ed5b673886951c8fb6cca
ivanova-12 <ivanov.sim-1977@yandex.ru>
2026-03-11 20:24:06 +0700

    Update practicum9_task6.py

--- practicum9_task6.py
+++ practicum9_task6.py
@@ -1,4 +1,4 @@
-with open('all_f.txt', 'r', encoding='utf-8') as f:
+with open('input.txt', 'r', encoding='utf-8') as f:
     number = f.readline()
     try:
         number = int(number)
@@ -6,11 +6,12 @@ with open('all_f.txt', 'r', encoding='utf-8') as f:
         print('ERROR')
         exit()
     all_other_lines = f.readlines()
-    with open('input.txt', 'w', encoding='utf-8') as fi:
+    with open('output.txt', 'w', encoding='utf-8') as fi:
         if len(all_other_lines) == number:
             fi.write('YES')
         else:
             fi.write('NO')
+
 
 
 
